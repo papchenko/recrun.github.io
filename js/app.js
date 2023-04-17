1b78f118d534eb487141ad3b35da2616ed5a0deb
nikopapchenko <nikopapchenko@gmail.com>
2023-04-17 09:21:53 +0300

    add popup (#5)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
+/*
+    imports all scripts
+*/
+
+// menu
 import { menu } from "./scripts/menu.js";
 menu();
-
+// scroll
 import { ssMoveTo } from "./scripts/scroll.js";
 ssMoveTo();
+// popup
+import { popup } from "./scripts/popup.js";
+popup();
---
+++ js/scripts/popup.js
@@ -0,0 +1,19 @@
+function popup() {
+    const clickLink = document.getElementById("clickLink");
+    const popup = document.getElementById("popup");
+    const closePopup = document.getElementById("closePopup");
+    
+    clickLink.addEventListener('click', ()=>{
+        popup.style.display = 'block';
+    });
+    closePopup.addEventListener('click', ()=>{
+        popup.style.display = 'none';
+    });
+    popup.addEventListener('click', ()=>{
+        popup.style.display = 'none';
+    });
+}
+
+export {
+    popup
+}
