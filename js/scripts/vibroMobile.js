7d967c3f41ca9e127047c0fef5f5774567bde807
nikopapchenko <nikopapchenko@gmail.com>
2023-04-20 10:29:51 +0300

    add swiper on lens-page (#10)

--- js/scripts/vibroMobile.js
+++ js/scripts/vibroMobile.js
@@ -14,7 +14,7 @@ const media = matchMedia('(max-width: 767px)');
   if(media.matches) {
     for (let i = 0; i < vibro.length; i++) {
       vibro[i].addEventListener('click', (e) => {
-      navigator.vibrate(2);
+      navigator.vibrate(4);
       });
     }
   }
